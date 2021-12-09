7c3f114feb1a7daee20178e40f78552af29637ef
Guilherme Mendes de Freitas Mariano <gmariano1@live.com>
2021-12-09 15:11:19 -0300

    Inicializa com bootstrap

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php require_once './components/header.php'; ?>
+<h1>Hello World</h1>
+<button type="button" class="btn btn-primary">Primary</button>
+<?php require_once './components/footer.php'; ?>
