0c2a43752d5d3be47d45d4a7e33a2d42d9bb2c35
Guilherme Mendes de Freitas Mariano <gmariano1@live.com>
2021-12-13 23:19:01 -0300

    DIR arrumado

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<?php require_once './components/header.php'; ?>
+<?php require_once __DIR__. '/components/header.php'; ?>
 
-<?php require_once './components/footer.php'; ?>
+<?php require_once __DIR__.'/components/footer.php'; ?>
